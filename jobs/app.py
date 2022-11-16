70846a3c41627461c3d44c2df38984979e08437a
masonleonhart <mason.leonhart@gmail.com>
2022-11-16 15:31:55 -0600

    amend open_connection function

--- jobs/app.py
+++ jobs/app.py
@@ -10,7 +10,7 @@ def open_connection():
     connection = getattr(g, '_connection', None)
 
     if connection is None:
-        connection = g['_connection'] = sqlite3.connect(PATH)
+        connection, g['_connection'] = sqlite3.connect(PATH)
 
 
 @app.route("/")
